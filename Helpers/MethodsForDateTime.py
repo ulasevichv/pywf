fd62e9b3180bc456bb0365a3d8f4cedee7f400c9
ulasevichv <ulasevichvictor@gmail.com>
2024-05-04 22:55:17 +0400

    Adding various methods for DateTime

--- Helpers/MethodsForDateTime.py
+++ Helpers/MethodsForDateTime.py
@@ -1,6 +1,37 @@
+from datetime import datetime, timezone
+
+
 class MethodsForDateTime:
     @classmethod
-    def getCurrentTimestampStr(cls):
-        from datetime import datetime, timezone
+    def getCurrentTimestamp(cls) -> int:
+        import math
+        return math.floor(datetime.now(timezone.utc).timestamp())
 
+    @classmethod
+    def getCurrentTimestampMS(cls) -> float:
+        return round(datetime.now(timezone.utc).timestamp(), 3)
+
+    @classmethod
+    def getCurrentTimestampMCS(cls) -> float:
+        return datetime.now(timezone.utc).timestamp()
+
+    @classmethod
+    def getCurrentTimestampStr(cls) -> str:
         return datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+
+    @classmethod
+    def getCurrentTimestampStrMS(cls) -> str:
+        dt = datetime.now(timezone.utc)
+        milliseconds = round(dt.microsecond / 1000)
+        return dt.strftime('%Y-%m-%d %H:%M:%S') + '.' + str(milliseconds)
+
+    @classmethod
+    def utcTimestampToStr(cls, timestamp: float | int):
+        dt = datetime.fromtimestamp(timestamp / 1000, timezone.utc)
+        return dt.strftime('%Y-%m-%d %H:%M:%S')
+
+    @classmethod
+    def utcTimestampToStrMS(cls, timestamp: float | int):
+        dt = datetime.fromtimestamp(timestamp / 1000, timezone.utc)
+        milliseconds = round(dt.microsecond / 1000)
+        return dt.strftime('%Y-%m-%d %H:%M:%S') + '.' + str(milliseconds)
